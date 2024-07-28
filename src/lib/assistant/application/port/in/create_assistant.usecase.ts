11ba75313347050fc1e52d02fad1744e774d1b08
Eklemis Santo Ndun <Eklemis.ndun@gmail.com>
2024-07-28 10:13:16 +0800

    Fixed issue on Assistant domain files not found

--- src/lib/assistant/application/port/in/create_assistant.usecase.ts
+++ src/lib/assistant/application/port/in/create_assistant.usecase.ts
@@ -1,5 +1,5 @@
-import { Assistant } from "./assistant";
-import { CreateAssistantCommand } from "./create_assistant.command";
+import { Assistant } from "@/lib/assistant/domain/assistant";
+import { CreateAssistantCommand } from "@/lib/assistant/domain/create_assistant.command";
 
 export interface CreateAssistantUseCase {
 	createAssistant(command: CreateAssistantCommand): Promise<Assistant>;
