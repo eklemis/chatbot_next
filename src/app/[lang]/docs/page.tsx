5b5b4978bcd718f089bcd4bfe315670840a635ef
Eklemis Santo Ndun <Eklemis.ndun@gmail.com>
2024-07-29 07:12:40 +0800

    Fixed circle size and position issue on docs

--- src/app/[lang]/docs/page.tsx
+++ src/app/[lang]/docs/page.tsx
@@ -11,44 +11,44 @@ export default async function Page({ params }: { params: { lang: string } }) {
 		<section className="flex flex-col items-center justify-center gap-y-4 p-4">
 			<h1 className="text-3xl font-bold">Milestones</h1>
 			<div className="flex flex-col md:flex-row gap-5 items-start justify-between">
-				<section className="flex flex-col gap-y-2 p-8 rounded w-full bg-gray-50 shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]">
+				<section className="flex flex-col gap-y-2 p-8 rounded w-full bg-gray-50 shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px]">
 					<h2 className="text-xl font-bold">Completed</h2>
 					<ul className="flex flex-col gap-y-1">
-						<li className="flex items-center gap-x-1 p-2 rounded bg-slate-200 text-sm px-4">
-							<CheckCircle2 className="w-4 h-4 text-gray-500" />
+						<li className="flex items-start gap-x-1 p-2 rounded bg-slate-200 text-sm px-4">
+							<CheckCircle2 className="min-w-5 min-h-5 w-5 h-5 text-green-600" />
 							Create Assistant
 						</li>
-						<li className="flex items-center gap-x-1 p-2 rounded bg-slate-200 text-sm px-4">
-							<CheckCircle2 className="w-4 h-4 text-gray-500" />
+						<li className="flex items-start gap-x-1 p-2 rounded bg-slate-200 text-sm px-4">
+							<CheckCircle2 className="min-w-5 min-h-5 w-5 h-5 text-green-600" />
 							Interview with an Assistant
 						</li>
-						<li className="flex items-center gap-x-1 p-2 rounded bg-slate-200 text-sm px-4">
-							<CheckCircle2 className="w-4 h-4 text-gray-500" />
+						<li className="flex items-start gap-x-1 p-2 rounded bg-slate-200 text-sm px-4">
+							<CheckCircle2 className="min-w-5 min-h-5 w-5 h-5 text-green-600" />
 							Assistant provide summary
 						</li>
 					</ul>
 				</section>
-				<section className="flex flex-col gap-y-2 p-8 rounded w-full bg-gray-50 shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]">
+				<section className="flex flex-col gap-y-2 p-8 rounded w-full bg-gray-50 shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px]">
 					<h2 className="text-xl font-bold">Remaining</h2>
 					<ul className="flex flex-col gap-y-1">
-						<li className="flex items-center gap-x-1 p-2 rounded bg-slate-200 text-sm px-4">
-							<Circle className="w-4 h-4 text-gray-500" />
-							Change and Delete Assistants
-						</li>
-						<li className="flex items-center gap-x-1 p-2 rounded bg-slate-200 text-sm px-4">
-							<Circle className="w-4 h-4 text-gray-500" />
+						<li className="flex items-start gap-x-1 p-2 rounded bg-slate-200 text-sm px-4">
+							<Circle className="min-w-5 min-h-5 w-5 h-5 text-gray-500" />
 							Save interview summary result
 						</li>
-						<li className="flex items-center gap-x-1 p-2 rounded bg-slate-200 text-sm px-4">
-							<Circle className="w-4 h-4 text-gray-500" />
+						<li className="flex items-start gap-x-1 p-2 rounded bg-slate-200 text-sm px-4">
+							<Circle className="min-w-5 min-h-5 w-5 h-5 text-gray-500" />
 							Listing interview results
 						</li>
-						<li className="flex items-center gap-x-1 p-2 rounded bg-slate-200 text-sm px-4">
-							<Circle className="w-4 h-4 text-gray-500" />
+						<li className="flex items-start gap-x-1 p-2 rounded bg-slate-200 text-sm px-4">
+							<Circle className="min-w-5 min-h-5 w-5 h-5 text-gray-500" />
+							Change and Delete Assistants
+						</li>
+						<li className="flex items-start gap-x-1 p-2 rounded bg-slate-200 text-sm px-4">
+							<Circle className="min-w-5 min-h-5 w-5 h-5 text-gray-500" />
 							User authentication for RBAC implementation
 						</li>
-						<li className="flex items-center gap-x-1 p-2 rounded bg-slate-200 text-sm px-4">
-							<Circle className="w-4 h-4 text-gray-500" />
+						<li className="flex items-start gap-x-1 p-2 rounded bg-slate-200 text-sm px-4">
+							<Circle className="min-w-5 min-h-5 w-5 h-5 text-gray-500" />
 							Integrate with other CMS
 						</li>
 					</ul>
