a36cd9ddf8918b7e21dcf6d7567af094399d0a50
Eklemis Santo Ndun <Eklemis.ndun@gmail.com>
2024-07-28 10:18:17 +0800

    Fixed issue on Interview interface not found

--- src/lib/interview/application/port/in/create_interview.usecase.ts
+++ src/lib/interview/application/port/in/create_interview.usecase.ts
@@ -1,4 +1,4 @@
-import { Interview } from "./interview";
+import { Interview } from "@/lib/interview/domain/interview";
 export interface CreateInterviewUseCase {
 	createInterviewSession(): Promise<Interview>;
 }
