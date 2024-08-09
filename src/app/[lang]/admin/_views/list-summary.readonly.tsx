a501d1e53a369bfad9dbe283e8b2c32c10fdd228
Eklemis Santo Ndun <Eklemis.ndun@gmail.com>
2024-08-09 17:50:19 +0700

    improved load list of saved interviews ux

--- src/app/[lang]/admin/_views/list-summary.readonly.tsx
+++ src/app/[lang]/admin/_views/list-summary.readonly.tsx
@@ -87,7 +87,7 @@ export function SummaryList({ dictionary, currPage, totalPage }: Params) {
 				<section className="w-full flex flex-col items-center justify-center mt-16">
 					<h2 className="text-2xl font-bold">Interview Summaries</h2>
 					<ul className="flex gap-4 p-4 px-0 w-full max-w-7xl flex-wrap items-center justify-center mt-4">
-						{summaryList.map((list, idx) => {
+						{summaryList.map((list: Summary, idx) => {
 							return (
 								<li
 									className="p-6 rounded-md overflow-clip bg-gradient-to-br to-amber-50 via-rose-50 from-white items-between w-64 max-w-64 h-44 max-h-44 shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px]"
@@ -95,14 +95,14 @@ export function SummaryList({ dictionary, currPage, totalPage }: Params) {
 								>
 									<Link
 										className=" h-full w-full flex flex-col gap-y-2"
-										href={"/admin/summaries/" + list?.id}
+										href={"/admin/summaries/" + list.id}
 									>
 										<h3 className="text-sm font-bold text-grey-700 flex h-11 min-h-11 line-clamp-2">
 											{list?.title}
 										</h3>
 
 										<p className="text-[12px] line-clamp-4 text-gray-600">
-											<ReactMarkdown>{list?.summary}</ReactMarkdown>
+											<ReactMarkdown>{list.summary}</ReactMarkdown>
 										</p>
 									</Link>
 								</li>
